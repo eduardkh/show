b564ae36920f26697505634e5e3ed646345336ef
Eduard Khiaev <eddietheone1984@gmail.com>
2023-01-13 20:51:21 +0200

    show ip interface brief

---
+++ cmd/brief.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/bi-zone/wmi"
+	"github.com/spf13/cobra"
+)
+
+type brief_win32_NetworkAdapterConfiguration struct {
+	IPAddress   []string
+	IPSubnet    []string
+	IPEnabled   bool
+	MACAddress  string
+	Description string
+}
+
+// briefCmd represents the brief command
+var briefCmd = &cobra.Command{
+	Use:   "brief",
+	Short: "get a brief interface related information",
+	Long:  `get a brief interface related information`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// fmt.Println("brief called")
+		var dst []brief_win32_NetworkAdapterConfiguration
+
+		q := wmi.CreateQueryFrom(&dst, "Win32_NetworkAdapterConfiguration", "")
+		if err := wmi.Query(q, &dst); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("IP Address\tSubnet Mask\tMAC Address\t\tIP Enabled\tInterface Description")
+		for _, v := range dst {
+			if v.IPEnabled {
+				fmt.Printf("%v\t%v\t%v\t%v\t\t%v\n", v.IPAddress[0], v.IPSubnet[0], v.MACAddress, v.IPEnabled, v.Description)
+			}
+		}
+	},
+}
+
+func init() {
+	interfaceCmd.AddCommand(briefCmd)
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/eduardkh/show
+
+go 1.19
+
+require (
+	github.com/bi-zone/wmi v1.1.4
+	github.com/spf13/cobra v1.6.1
+)
+
+require (
+	github.com/bi-zone/go-ole v1.2.5 // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/scjalliance/comshim v0.0.0-20190308082608-cf06d2532c4e // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20200806060901-a37d78b92225 // indirect
+)
