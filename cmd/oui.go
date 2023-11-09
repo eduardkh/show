aa13313609cf9ec37de90efa4dc6daf4abfe56d7
Eduard Khiaev <eddietheone1984@gmail.com>
2023-11-09 22:36:34 +0200

    show ip oui - query

--- cmd/oui.go
+++ cmd/oui.go
@@ -4,11 +4,13 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -62,39 +64,120 @@ func downloadFile(filepath string, url string) error {
 	return err
 }
 
+// PrintOUIInfo searches the OUI CSV file for the given MAC address and prints its information.
+func PrintOUIInfo(macAddress string) error {
+	// Standardize the MAC address by removing delimiters and converting to uppercase
+	macAddress = strings.ToUpper(macAddress)
+	macAddress = strings.NewReplacer(":", "", "-", "", ".", "").Replace(macAddress)
+
+	// Now that we've cleaned the MAC address, check if it's at least 6 characters long
+	if len(macAddress) < 6 {
+		return fmt.Errorf("MAC address must be at least 6 characters after standardizing")
+	}
+
+	// Trim the MAC address to the first 6 characters (OUI portion)
+	ouiPortion := macAddress[:6]
+
+	// Get the local application data directory
+	appDataDir, err := os.UserConfigDir()
+	if err != nil {
+		return err
+	}
+
+	// Construct the path to the oui.csv file
+	csvFilePath := filepath.Join(appDataDir, "MyOUIApp", "oui.csv")
+
+	// Open the CSV file
+	file, err := os.Open(csvFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Create a new CSV reader
+	reader := csv.NewReader(file)
+
+	// Read and ignore the header line
+	if _, err := reader.Read(); err != nil {
+		return err
+	}
+
+	// Iterate through the CSV records and print the matching OUI information
+	found := false
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		// Check if the current record contains the OUI portion of the MAC address
+		if strings.HasPrefix(strings.ToUpper(record[1]), ouiPortion) {
+			fmt.Printf("MAC Address: \"%s\"\n", macAddress)
+			fmt.Printf("Vendor: \"%s\"\n", record[2])
+			found = true
+			break // Stop after finding the first match
+		}
+	}
+
+	if !found {
+		fmt.Println("No matching records found for the given MAC address.")
+	}
+
+	return nil
+}
+
 // ouiCmd represents the oui command
 var ouiCmd = &cobra.Command{
 	Use:   "oui",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Display vendor information for a given MAC address based on its OUI.",
+	Long: `This command retrieves and displays the manufacturer's details associated with the Organizationally Unique Identifier (OUI) segment of a provided MAC address.
+	It accepts various MAC address formats, normalizing them for processing.
+	This tool can be particularly useful for network diagnostics and hardware identification purposes.
+	
+	Usage:
+	show ip oui 005056C00001
+	show ip oui 00:50:56:C0:00:01
+	show ip oui 00-50-56-C0-00-01
+	show ip oui '0050.56C0.0001'
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		update, _ := cmd.Flags().GetBool("update")
 		if update {
+			// Existing logic for updating the OUI file
+
 			err := DownloadOUIFile()
 			if err != nil {
 				fmt.Println("Error:", err)
 			}
 		} else {
-			fmt.Println("oui called")
+			// Check if a MAC address argument is provided
+			if len(args) == 0 {
+				commands := []string{
+					"Usage:",
+					"show ip oui 005056C00001",
+					"show ip oui 00:50:56:C0:00:01",
+					"show ip oui 00-50-56-C0-00-01",
+					"show ip oui '0050.56C0.0001'",
+				}
+				for _, command := range commands {
+					fmt.Println(command)
+				}
+				return
+			}
+			macAddress := args[0] // assuming the MAC address is the first argument
+			err := PrintOUIInfo(macAddress)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
 		}
 	},
 }
 
 func init() {
 	ipCmd.AddCommand(ouiCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// ouiCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	ouiCmd.Flags().BoolP("update", "u", false, "update OUI file")
 }
